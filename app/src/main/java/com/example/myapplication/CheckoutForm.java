4081802a32ac782b381999eaf0387e410b310407
rohit-vasnani <rohitvasnani@gmail.com>
2023-04-05 15:12:38 +0200

    adding activity layout

--- app/src/main/java/com/example/myapplication/CheckoutForm.java
+++ app/src/main/java/com/example/myapplication/CheckoutForm.java
@@ -1,6 +1,16 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 
 public class CheckoutForm extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout_form);
+
+    }
 }
